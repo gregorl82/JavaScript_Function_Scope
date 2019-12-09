c8be6bea89071154494647434e0d608fa53d7898
Gregor Lennie <gregorlennie@hotmail.co.uk>
2019-12-09 19:04:17 +0000

    Adds answer for Episode 4

--- whodunnit.js
+++ whodunnit.js
@@ -56,3 +56,23 @@ console.log('First Verdict: ', firstVerdict);
 
 const secondVerdict = `The murderer is ${murderer}.`;
 console.log('Second Verdict: ', secondVerdict);
+
+/*
+Episode 4
+The output will be 'The suspects are Miss Scarlet, Professor Plum, Colonel Mustard.'
+then 'Suspect three is Mrs. Peacock.', because the suspectThree variable is only
+changed inside the declareAllSuspects() function scope. The suspectThree declared
+at the start of the program remains unchanged.
+*/
+let suspectOne = 'Miss Scarlet';
+let suspectTwo = 'Professor Plum';
+let suspectThree = 'Mrs. Peacock';
+
+const declareAllSuspects = function() {
+  let suspectThree = 'Colonel Mustard';
+  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+}
+
+const suspects = declareAllSuspects();
+console.log(suspects);
+console.log(`Suspect three is ${suspectThree}.`);
