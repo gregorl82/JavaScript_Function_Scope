5da2c31de3d275da5db7a45a221b774efaba8202
Gregor Lennie <gregorlennie@hotmail.co.uk>
2019-12-09 18:44:22 +0000

    Adds answer for Episode 2

--- whodunnit.js
+++ whodunnit.js
@@ -12,3 +12,24 @@ const declareMurderer = function() {
 
 const verdict = declareMurderer();
 console.log(verdict);
+
+// The output will be 'The murderer is Miss Scarlet' because the scenario variable
+// is declared in the main program and is available to the declareMurderer() function.
+
+// Episode 2
+
+const murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// This function will give an error, since murderer is a constant and is immutable.
