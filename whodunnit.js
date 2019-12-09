77d2dc8ca2b3debfe9a147d455d32c902b0b04be
Gregor Lennie <gregorlennie@hotmail.co.uk>
2019-12-09 18:37:59 +0000

    Initial commit

---
+++ whodunnit.js
@@ -0,0 +1,14 @@
+// Episode 1
+
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Library',
+  weapon: 'Rope'
+};
+
+const declareMurderer = function() {
+  return `The murderer is ${scenario.murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
